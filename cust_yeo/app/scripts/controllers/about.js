e7c17cd79f40219a16fe105649c64d31309c51ce
genkidama99 <genkidama99@gmail.com>
2015-01-06 00:10:41 +0900

    angular_generator展開
    angular_generatorを展開したままのフォルダ

---
+++ cust_yeo/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name custYeoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the custYeoApp
+ */
+angular.module('custYeoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
